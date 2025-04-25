87812bce0a914199e301a2fc534541d4f010c12c
noahmjanssen <noahmjanssen@gmail.com>
2025-04-25 12:11:36 +0200

    coverage report screenshot

--- src/test/java/data_management/FileDataReaderTest.java
+++ src/test/java/data_management/FileDataReaderTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class FileDataReaderTest {
     @Test
